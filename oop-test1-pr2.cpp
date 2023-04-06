a02d42742494e0748125b0e7f151e35795de1285
KristiyanHub <117939297+KristiyanHub@users.noreply.github.com>
2023-04-06 21:07:23 +0300

    Update oop-test1-pr2.cpp

--- oop-test1-pr2.cpp
+++ oop-test1-pr2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <cstring>
 
 struct player
 {
@@ -7,11 +8,12 @@ struct player
     unsigned score;
 };
 
-player *playersList;
+const int MAX = 100;
+player playersList[MAX];
 
-void loadFromFile(player* playersList)
+void loadFromFile(player *playersList)
 {
-    std::ofstream readPlayers("players.dat", std::ios::binary);
+    std::ifstream readPlayers("players.dat", std::ios::binary);
 
     if (!readPlayers.is_open())
     {
@@ -21,13 +23,97 @@ void loadFromFile(player* playersList)
 
     int i = 0;
 
+    unsigned scoreCurrent;
+
+    char null[2] = "0";
+
     while (!readPlayers.eof())
     {
-        readPlayers << playersList[i].nickname;
-        readPlayers << playersList[i].score;
+        readPlayers >> playersList[i].nickname;
+        readPlayers >> scoreCurrent;
+        if (scoreCurrent > 5000)
+        {
+            std::cout << "Ivalid score, player will be ingonerd \n";
+            strcpy_s(playersList[i].nickname, null);
+            playersList[i].score = 0;
+        }
+        else
+        {
+            playersList[i].score = scoreCurrent;
+        }
 
         i += 1;
     }
+
+    readPlayers.close();
+}
+
+void checkName(player* playersList, char* nick)
+{
+    int swapScores;
+
+    for (int i = 0; i < MAX; i++)
+    {
+        for (int j = 0; j < strlen(playersList[i].nickname); j++)
+        {
+            if (playersList[i].score != 0)
+            {
+                if (playersList[i].nickname[j] < nick[j])
+                {
+                    std::cout << playersList[i].nickname;
+                    std::cout << " ";
+                    std::cout << playersList[i].score;
+                    std::cout << " ";
+                    break;
+                }
+            }
+        }
+    }
+}
+
+void sortByScore(player* playersList, char *nick)
+{
+    unsigned swapScores;
+    char swapNames[21];
+
+    for (int i = 0; i < MAX; i++)
+    {
+        for (int j = i + 1; j < MAX - 1; j++)
+        {
+            
+            if (playersList[i].score < playersList[j].score)
+            {
+                if (playersList[i].score != 0)
+                {
+                    //swap name
+                    strcpy_s(swapNames, playersList[i].nickname);
+                    strcpy_s(playersList[i].nickname, playersList[j].nickname);
+                    strcpy_s(playersList[j].nickname, swapNames);
+
+                    //swap score
+                    swapScores = playersList[i].score;
+                    playersList[i].score = playersList[j].score;
+                    playersList[j].score = swapScores;
+                }
+
+            }
+        }
+    }
+
+    for (int i = 0; i < MAX; i++)
+    {
+        if (playersList[i].score != 0)
+        {
+            std::cout << playersList[i].nickname;
+            std::cout << " ";
+            std::cout << playersList[i].score;
+            std::cout << " ";
+        }
+
+    }
+    std::cout << "\n";
+    std::cout << "Sorted by the word you entered: \n";
+    checkName(playersList, nick);
 }
 
 int main()
@@ -38,9 +124,19 @@ int main()
     char nick[21];
     std::cin.getline(nick, 21);
 
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < MAX; i++)
     {
-        std::cout << playersList[i].nickname;
-        std::cout << playersList[i].score;
+        if (playersList[i].score != 0)
+        {
+            std::cout << playersList[i].nickname;
+            std::cout << " ";
+            std::cout << playersList[i].score;
+            std::cout << " ";
+        }
+
     }
+    std::cout << "\n";
+    
+    std::cout << "Sorted by score: \n";
+    sortByScore(playersList, nick);
 }
